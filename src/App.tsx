2bd5af58ea2a6501432f310054aaacab635c8b8b
Prathamesh Joshi <prathamesh231197@gmail.com>
2023-06-02 18:57:22 +0530

    hooks service of usegame with an issue of fetching games after refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
-import NavBar from "./components/navbar";
+import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 
 function App() {
